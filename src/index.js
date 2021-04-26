d09dacb3c286e4d3897e770199faf79251f3910d
Gokul <ssgokul22@gmail.com>
2021-04-26 16:33:23 +0530

    checkpoint 2 :  Creating the Header Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from "./assests/common/header/Header.js";
+
 ReactDOM.render(
     <div>
-
-
+  <Header/>
     </div>,
 
   document.getElementById('root')
